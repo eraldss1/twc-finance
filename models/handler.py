1ea99bae66b51ceb29a0cb8dc5c4b1f5078827d0
m e s s <erald.muhammad@gmail.com>
2023-11-03 16:14:00 +0700

    fix import

--- models/handler.py
+++ models/handler.py
@@ -1,16 +1,17 @@
-import watchdog.events
 import os
 import time
 
+import watchdog.events
 
 
 class Handler(watchdog.events.PatternMatchingEventHandler):
     driver = None
 
     def __init__(self, **kwargs):
-        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=[
-            '*.xlsx'], ignore_directories=True, case_sensitive=False)
-        self.driver = kwargs['driver']
+        watchdog.events.PatternMatchingEventHandler.__init__(
+            self, patterns=["*.xlsx"], ignore_directories=True, case_sensitive=False
+        )
+        self.driver = kwargs["driver"]
 
     # Handle the new added file
     def on_created(self, event):
@@ -19,5 +20,5 @@ class Handler(watchdog.events.PatternMatchingEventHandler):
         file_name = str(os.path.basename(event.src_path))
         if file_name.startswith("TWCFIN"):
             self.driver.process_file(event.src_path)
-        
+
         time.sleep(5)
